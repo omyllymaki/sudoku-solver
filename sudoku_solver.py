9c765cbc315a8aa68392d95cae2899687c07243c
Ossi Myllymäki <omyllymaki@gmail.com>
2019-12-08 11:44:05 +0200

    Update sudoku solver. Now it can solve any sudoku.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,90 +1,19 @@
+import time
+
 import numpy as np
 import pandas as pd
 
-PATH = "sudoku_medium.csv"
-
-
-class SudokuSolver:
-
-    def __init__(self):
-        self.group_table = self.generate_group_table()
-        self.possible_values = set(range(1, 10))
-
-    def solve(self, table):
-        self.table = table
-
-        self.fill_certain()
-        if self.is_sudoku_solved():
-            return self.table
-
-    def get_cell_with_lowest_number_of_possible_values(self):
-        min_possible_values = 0
-        r_out, c_out = None, None
-
-        for r in range(9):
-            for c in range(9):
-                element = self.table[r, c]
-                if not np.isnan(element):
-                    continue
-                possible_values = self.get_possible_values(r, c)
-                if len(possible_values) < min_possible_values:
-                    r_out, c_out = r, c
-                    min_possible_values = len(possible_values)
-        return r_out, c_out
-
-    def fill_certain(self):
-        n_nans_previous = sum(sum(np.isnan(self.table)))
-        while True:
-
-            for r in range(9):
-                for c in range(9):
-                    element = self.table[r, c]
-                    if not np.isnan(element):
-                        continue
-
-                    possible_values = self.get_possible_values(r, c)
-                    if len(possible_values) == 1:
-                        table[r, c] = possible_values[0]
-
-            n_nans = sum(sum(np.isnan(table)))
-            if (n_nans == 0) or (n_nans == n_nans_previous):
-                return table
-            else:
-                n_nans_previous = n_nans
-
-    def get_possible_values(self, r, c):
-        row = self.table[r, :]
-        row_values = row[~np.isnan(row)]
-        column = self.table[:, c]
-        column_values = column[~np.isnan(column)]
-
-        element_group = self.group_table[r, c]
-        block = self.table[self.group_table == element_group]
-        block_values = block[~np.isnan(block)]
-
-        forbidden_values = set(row_values) | set(column_values) | set(block_values)
-        possible_values = self.possible_values - forbidden_values
-
-        return list(possible_values)
-
-    def is_sudoku_solved(self):
-        if sum(sum(np.isnan(self.table))) == 0:
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def generate_group_table():
-        group = 0
-        group_table = np.empty((0, 9))
-        for i in range(3):
-            row_table = np.empty((3, 0))
-            for j in range(3):
-                block = group * np.ones((3, 3))
-                group += 1
-                row_table = np.hstack((row_table, block))
-            group_table = np.vstack((group_table, row_table))
-        return group_table
+GROUP_TABLE = np.array([
+    [0, 0, 0, 1, 1, 1, 2, 2, 2],
+    [0, 0, 0, 1, 1, 1, 2, 2, 2],
+    [0, 0, 0, 1, 1, 1, 2, 2, 2],
+    [3, 3, 3, 4, 4, 4, 5, 5, 5],
+    [3, 3, 3, 4, 4, 4, 5, 5, 5],
+    [3, 3, 3, 4, 4, 4, 5, 5, 5],
+    [6, 6, 6, 7, 7, 7, 8, 8, 8],
+    [6, 6, 6, 7, 7, 7, 8, 8, 8],
+    [6, 6, 6, 7, 7, 7, 8, 8, 8]
+])
 
 
 def validate_table(table):
@@ -95,29 +24,96 @@ def validate_table(table):
         raise Exception(f"Invalid table height: {h}")
 
     for i in range(9):
-        row = table[i, :]
-        row_values = row[~np.isnan(row)]
+        row_values = table[i, :]
+        row_values = row_values[row_values != 0]
         if len(row_values) != len(set(row_values)):
             raise Exception(f"Row {i} contains duplicate elements")
 
     for i in range(9):
-        column = table[:, i]
-        column_values = column[~np.isnan(column)]
+        column_values = table[:, i]
+        column_values = column_values[column_values != 0]
         if len(column_values) != len(set(column_values)):
             raise Exception(f"Column {i} contains duplicate elements")
 
     unique_values = np.unique(table)
-    unique_values = unique_values[~np.isnan(unique_values)]
     for value in unique_values:
-        if value not in list(range(1, 10)):
+        if value not in list(range(0, 10)):
             raise Exception(f"Table contains invalid value: {value}")
 
 
+def print_board(table):
+    print(np.array(table))
+
+
+def find_empty_cells(table):
+    return np.argwhere(table == 0)
+
+
+def get_possible_values(table, r, c):
+    row_values = table[r, :]
+    column_values = table[:, c]
+
+    element_group = GROUP_TABLE[r, c]
+    block_values = table[GROUP_TABLE == element_group]
+
+    forbidden_values = set(row_values) | set(column_values) | set(block_values)
+    possible_values = set(range(1, 10)) - forbidden_values
+
+    return list(possible_values)
+
+
+def create_book_keeper(table):
+    empty_cells = find_empty_cells(table)
+    book_keeper = []
+    for empty_cell in empty_cells:
+        item = (empty_cell[0], empty_cell[1], 0)
+        book_keeper.append(item)
+    return book_keeper
+
+
+def solve(table):
+    book_keeper = create_book_keeper(table)
+
+    counter, index = 0, 0
+    while True:
+
+        if index == len(book_keeper):
+            print(f"Solution took {counter} rounds")
+            return True
+
+        if index < 0:
+            print(f"Solution cannot be found")
+            return False
+
+        counter += 1
+
+        r, c, current_value = book_keeper[index]
+        possible_values = get_possible_values(table, r, c)
+        possible_values = [v for v in possible_values if v > current_value]
+        if not possible_values:
+            book_keeper[index] = (r, c, 0)
+            table[r, c] = 0
+            index -= 1
+            continue
+
+        new_value = min(possible_values)
+        book_keeper[index] = (r, c, new_value)
+        table[r, c] = new_value
+        index += 1
+
+
+PATH = "sudoku_hardest.csv"
 df_table = pd.read_csv(PATH, header=None, index_col=False)
 table = df_table.to_numpy()
+table = np.nan_to_num(table)
+table = table.astype(int)
 validate_table(table)
-print(table)
 
-solver = SudokuSolver()
-table = solver.solve(table)
 print(table)
+time_start = time.time()
+solve(table)
+time_end = time.time()
+duration = time_end - time_start
+print("-------------------")
+print(table)
+print(f"Solution took {duration:.3f} seconds")
